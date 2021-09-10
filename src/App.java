1fb63d63b128b2057e5041a9c11e363f99cbbc78
v4z <luizsamuelmaciel@gmail.com>
2021-09-10 10:20:22 -0300

    commit one

--- src/App.java
+++ src/App.java
@@ -1,8 +1,8 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args)   {
+        System.out.println("BOM DIA É SEXTA FEIRA KRAIKKKKKKKKK");
       
-
+        System.out.println ("teste aaaa");
 
           
 
